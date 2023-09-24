8b7bbcaa1d782264acc9686d20d9b4b82275dafc
gelono <gelonooz@gmail.com>
2023-09-24 13:41:48 +0300

    fix settings.py

--- comments_project/settings.py
+++ comments_project/settings.py
@@ -161,4 +161,4 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = os.environ.get("DEFAULT_FILE_STORAGE")
